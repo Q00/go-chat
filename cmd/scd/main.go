62b2e4759b9216c451b7588280ecc59a46b81e71
Q00 <jqyu.lee@gmail.com>
2023-12-08 14:17:19 +0900

    feat: add main.go

---
+++ cmd/scd/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"github.com/GoodOnuii-B/seoltab-chat/config"
+	"github.com/GoodOnuii-B/seoltab-chat/graphql"
+	"github.com/GoodOnuii-B/seoltab-chat/graphql/docs"
+	graphqlGo "github.com/graph-gophers/graphql-go"
+	"github.com/graph-gophers/graphql-go/relay"
+	"github.com/graph-gophers/graphql-transport-ws/graphqlws"
+	"github.com/labstack/echo/v4"
+	m "github.com/labstack/echo/v4/middleware"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const (
+	PORT = 8080
+)
+
+func main() {
+	env := os.Getenv("GO_ENV")
+	fmt.Printf("chat node starts, env:  %v\n", env)
+	// load config
+	cfg := config.LoadConfig()
+
+	e := echo.New()
+	schemaCompiled := graphqlGo.MustParseSchema(graphql.Schema, graphql.NewResolver(&cfg.AWS))
+	graphqlHandler := graphqlws.NewHandlerFunc(schemaCompiled, &relay.Handler{Schema: schemaCompiled})
+
+	echoGraphqlHandler := func(c echo.Context) error {
+		graphqlHandler(c.Response().Writer, c.Request())
+		return nil
+	}
+
+	g := docs.NewGraphiql(PORT)
+	e.Use(m.Recover())
+	e.Use(CORS)
+	e.Any("/graphql", func(c echo.Context) error {
+		if c.Request().Header.Get("Upgrade") == "websocket" {
+			// Handle WebSocket GraphQL connection
+			return echoGraphqlHandler(c)
+		} else if c.Request().Method == "GET" {
+			return g.Start()(c)
+		} else {
+			// Handle regular HTTP GraphQL request
+			return echoGraphqlHandler(c)
+		}
+	})
+
+	server := &http.Server{
+		Addr:         fmt.Sprintf("0.0.0.0:%d", PORT),
+		ReadTimeout:  time.Second * 30,
+		WriteTimeout: time.Second * 30,
+		IdleTimeout:  time.Second * 120,
+	}
+
+	log.Fatal(e.StartServer(server))
+	log.Fatal(server.ListenAndServe())
+}
+
+// CORS allows us to customize the CORS headers
+func CORS(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		CORSMiddleware := m.CORSWithConfig(m.CORSConfig{
+			AllowOrigins: []string{"http://localhost:3000"},
+		})
+		CORSHandler := CORSMiddleware(next)
+		return CORSHandler(c)
+	}
+}
